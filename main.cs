ba98e83591450486244ed220ae097f45ab9f59cc
Sir-Vinicius <viniciusidai@gmail.com>
2023-11-04 16:36:49 -0300

    Atualizando Projeto

--- main.cs
+++ main.cs
@@ -4,7 +4,6 @@ class main
 {
     static void Main(string[] args)
     {
-        Supervisor presidente = new Presidente("Amanda Melo (Presidente)");
         Supervisor gerente1 = new Gerente("Maria Vitória (Gerente 1)");
         Supervisor gerente2 = new Gerente("Carlos Alberto (Gerente 2)");
         Funcionario caixa1 = new Caixa("Camila Arantes (Caixa 1)");
@@ -12,16 +11,12 @@ class main
         Funcionario atendente1 = new Atendente("Mariana Oliveira (Atendente 1)");
         Funcionario atendente2 = new Atendente("Rafael Lima (Atendente 2)");
 
-        presidente.Adicionar(gerente1);
-        presidente.Adicionar(gerente2);
-
         gerente1.Adicionar(caixa1);
         gerente1.Adicionar(caixa2);
 
         gerente2.Adicionar(atendente1);
         gerente2.Adicionar(atendente2);
 
-        presidente.ListarFuncionarios();
         gerente1.ListarFuncionarios();
         gerente2.ListarFuncionarios();
 
